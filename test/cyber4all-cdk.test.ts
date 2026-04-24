65881761ee303e39e427927163af9fa099805043
Christopher Wagner <christopherwagner@Christophers-MacBook-Pro.local>
2026-04-24 12:48:21 -0400

    initialize cdk application

---
+++ test/cyber4all-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cyber4AllCdk from '../lib/cyber4all-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cyber4all-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cyber4AllCdk.Cyber4AllCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
